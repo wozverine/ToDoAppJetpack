3861b439e35591831faaab0aa70ad477e338367a
wozverine <wozverine@users.noreply.github.com>
2024-09-07 18:57:21 +0300

    sqlite

--- app/src/main/java/com/glitch/todoapp/MainActivity.kt
+++ app/src/main/java/com/glitch/todoapp/MainActivity.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.activity.viewModels
 import com.glitch.todoapp.ui.theme.ToDoAppTheme
+import com.glitch.todoapp.uix.viewmodel.MainpageViewModel
+import com.glitch.todoapp.uix.viewmodel.TodoDetailViewModel
+import com.glitch.todoapp.uix.viewmodel.TodoRegisterViewModel
+import com.glitch.todoapp.uix.views.PageSwitch
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+	val todoRegisterViewModel: TodoRegisterViewModel by viewModels()
+	val todoDetailViewModel: TodoDetailViewModel by viewModels()
+	val mainpageViewModel: MainpageViewModel by viewModels()
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContent {
 			ToDoAppTheme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-				}
+				PageSwitch(mainpageViewModel, todoRegisterViewModel, todoDetailViewModel)
 			}
 		}
 	}
